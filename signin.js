deb1925dd00d16179638a5a90e9bd986d83c3a36
vijaykumar-123456 <vijaykumarbirra@gmail.com>
2024-08-01 19:56:57 +0530

    Add files via upload

---
+++ signin.js
@@ -0,0 +1,15 @@
+import React from "react"
+function signin() {
+    return 
+    <div className="flex flex-col items-center border-2 border-[black]">
+    <div className="flex border-[1px] border-[red]">
+<div>Image</div>
+<div>Form</div>
+
+    </div>
+    <div className="border-[1px] border-[green]">Footer</div>
+    
+    </div>
+}
+
+export default signin;
